529c421cc98ab6351f4a6832c0fc4b28d540e5a3
github yuya-lock <yuyahappy1009@gmail.com>
2022-03-04 13:36:19 +0900

    Create forms and admin

--- zinniaapp/admin.py
+++ zinniaapp/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth import get_user_model
+from .forms import UserChangeForm, UserCreationForm
 
-# Register your models here.
+
+User = get_user_model()
+
+
+class CustomizeUserAdmin(UserAdmin):
+    form = UserChangeForm
+    add_form = UserCreationForm
+
+    list_display = ('username', 'email', 'is_staff')
+
+    fieldsets = (
+        ('ユーザ情報', {'fields': ('username', 'email', 'password', 'website', 'picture')}),
+        ('パーミッション', {'fields': ('is_staff', 'is_active', 'is_superuser')}),
+    )
+
+    add_fieldsets = (
+        ('ユーザ情報', {
+            'fields': ('username', 'email', 'password', 'confirm_password')
+        }),
+    )
+
+
+admin.site.register(User, CustomizeUserAdmin)
