78421bd721a302969cf9fc2c51d91ea4ba52825c
github yuya-lock <yuyahappy1009@gmail.com>
2022-03-04 12:13:23 +0900

    Create custom manager and custom user classes

--- zinniaapp/models.py
+++ zinniaapp/models.py
@@ -1,3 +1,47 @@
 from django.db import models
+from django.contrib.auth.models import (
+    BaseUserManager, AbstractBaseUser, PermissionsMixin
+)
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+
+    def create_user(self, username, email, password=None):
+        if not email:
+            raise ValueError('メールアドレスは必須です。')
+        user = self.model(
+            username=username,
+            email=email
+        )
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, username, email, password=None):
+        user = self.model(
+            username=username,
+            email=email
+        )
+        user.set_password(password)
+        user.is_staff = True
+        user.is_active = True
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    username = models.CharField(max_length=150)
+    email = models.EmailField(max_length=255, unique=True)
+    is_active = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+    website = models.URLField(null=True)
+    picture = models.FileField(null=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
+    objects = UserManager()
+
+    def __str__(self):
+        return self.email
