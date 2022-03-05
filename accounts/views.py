953aa7db26a545ca9f5bf068cde493339dca2ea2
github yuya-lock <yuyahappy1009@gmail.com>
2022-03-06 02:33:28 +0900

    Change link

--- accounts/views.py
+++ accounts/views.py
@@ -2,8 +2,5 @@ from django.shortcuts import render
 from django.views.generic.base import TemplateView
 
 
-class IndexView(TemplateView):
-    template_name = 'index.html'
-
 class HomeView(TemplateView):
     template_name = 'home.html'
