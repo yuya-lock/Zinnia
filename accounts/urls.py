81d2589beeaad41d9165aa90a0832819cc4bc65e
github yuya-lock <yuyahappy1009@gmail.com>
2022-03-07 02:50:42 +0900

    Add various functions related to user

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,18 @@
 from django.urls import path
-from .views import HomeView
+
+from .views import (
+    HomeView, RegistUserView, UserLoginView, UserLogoutView, UserDetailView, activate_user,
+    UserUpdateView
+)
 
 
 app_name = 'accounts'
 urlpatterns = [
-    path('home/', HomeView.as_view(), name='home')
+    path('home/', HomeView.as_view(), name='home'),
+    path('regist/', RegistUserView.as_view(), name='regist'),
+    path('activate_user/<uuid:token>', activate_user, name='activate_user'),
+    path('user_login/', UserLoginView.as_view(), name='user_login'),
+    path('user_logout/', UserLogoutView.as_view(), name='user_logout'),
+    path('user_detail/<int:pk>', UserDetailView.as_view(), name='user_detail'),
+    path('user_update/<int:pk>', UserUpdateView.as_view(), name='user_update'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,71 @@
 from django.shortcuts import render
+from django.views.generic.edit import CreateView
+from django.views.generic.detail import DetailView
 from django.views.generic.base import TemplateView
+from django.views.generic.edit import UpdateView
+from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.core.files.storage import FileSystemStorage
+
+from .forms import RegistForm, UserLoginForm, UserUpdateForm
+from .models import UserActivateTokens, User
+import os
 
 
 class HomeView(TemplateView):
-    template_name = 'home.html'
+    template_name = os.path.join('accounts', 'home.html')
+
+
+class RegistUserView(CreateView):
+    template_name = os.path.join('accounts', 'regist.html')
+    form_class = RegistForm
+
+
+def activate_user(request, token):
+    user_activate_token = UserActivateTokens.objects.activate_user_by_token(token)
+    return render(
+        request, 'accounts/activate_user.html'
+    )
+
+
+class UserLoginView(LoginView):
+    template_name = os.path.join('accounts', 'user_login.html')
+    authentication_form = UserLoginForm
+
+    def form_valid(self, form):
+        remember = form.cleaned_data['remember']
+        if remember:
+            self.request.session.set_expiry(604800)
+        return super().form_valid(form)
+
+
+class UserLogoutView(LogoutView):
+    pass
+
+
+class UserDetailView(LoginRequiredMixin, DetailView):
+    model = User
+    template_name = os.path.join('accounts', 'user_detail.html')
+
+    def dispatch(self, *args, **kwargs):
+        return super().dispatch(*args, **kwargs)
+
+
+class UserUpdateView(UpdateView):
+    template_name = os.path.join('accounts', 'user_update.html')
+    
+    form_class = UserUpdateForm
+    model = User
+
+
+def upload_model_form(request):
+    user = None
+    if request.method == 'POST':
+        form = UserUpdateForm(request.POST, request.FILES)
+        if form.is_valid():
+            user = form.save()
+    else:
+        form = UserUpdateForm()
+    return render(request, 'accounts/user_update.html', context={
+        'form': form, 'user': user
+    })
